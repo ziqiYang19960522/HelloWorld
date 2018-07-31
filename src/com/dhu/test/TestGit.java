83af44ac95c73e24e0ce771541cb5bf97ba971fd
ziqiYang <919460668@qq.com>
2018-07-31 18:09:31 +0800

    version2

--- src/com/dhu/test/TestGit.java
+++ src/com/dhu/test/TestGit.java
@@ -3,5 +3,6 @@ package com.dhu.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("some words");
     }
 }
